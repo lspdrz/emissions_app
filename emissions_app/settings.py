026876007502d496ccf6799b94e606207f820a5d
Luis Pedraza <luispedraza@Luiss-MacBook-Pro.local>
2017-10-13 20:45:09 -0500

    changed static files settings

--- emissions_app/settings.py
+++ emissions_app/settings.py
@@ -95,11 +95,10 @@ USE_TZ = True
 #STATIC FILES
 ####
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'assets'),
+	os.path.join(BASE_DIR, 'static'),
 )
 
 ####
--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,11 @@ module.exports = {
     //the entry point we created earlier. Note that './' means
     //your current directory. You don't have to specify the extension  now,
     //because you will specify extensions later in the `resolve` section
-    entry: './assets/js/index',
+    entry: './static/js/index',
 
     output: {
         //where you want your compiled bundle to be stored
-        path: path.resolve('./assets/bundles/'),
+        path: path.resolve('./static/bundles/'),
         //naming convention webpack should use for your files
         filename: '[name]-[hash].js',
     },
