8f69e3aba977e8786b2f52dc2fdfbe5b5c795afb
Luis Pedraza <luispedraza@Luiss-MacBook-Pro.local>
2017-10-13 20:39:07 -0500

    typo in settings

--- emissions_app/settings.py
+++ emissions_app/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 #STATIC FILES
 ####
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
