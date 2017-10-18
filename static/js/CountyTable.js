1890adc6db7c73aec429ea2bb2a9b81aaa42dcb7
Luis Pedraza <luispedraza@Luiss-MacBook-Pro.local>
2017-10-18 13:43:16 -0500

    added stats, calculated sums for emissions

--- static/js/CountyTable.js
+++ static/js/CountyTable.js
@@ -5,9 +5,20 @@ export class CountyTable extends React.Component {
     super(props);
   }
 
+  round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
+
   render() {
     var county = this.props.county;
     var num = 0;
+    var co_tpy = 0;
+    var nox_tpy = 0;
+    var pb_tpy = 0;
+    var pm_ten_tpy = 0;
+    var pm_two_five_tpy = 0;
+    var so_two_tpy = 0;
+    var voc_tpy = 0;
     //Array to hold entries for the county selected
     var countyRows = [];
     //Go through entries and push all of the corresponding
@@ -19,6 +30,12 @@ export class CountyTable extends React.Component {
             {entry.county} - {entry.co_tpy}
           </li>);
         num++;
+        co_tpy += Number(entry.co_tpy);
+        nox_tpy += Number(entry.nox_tpy);
+        pb_tpy += Number(entry.pb_tpy);
+        pm_ten_tpy += Number(entry.pm_ten_tpy);
+        so_two_tpy += Number(entry.so_two_tpy);
+        voc_tpy += Number(entry.voc_tpy);
       }
     });
 
@@ -28,6 +45,31 @@ export class CountyTable extends React.Component {
         <h1>{this.props.county}</h1>
         <p>{this.props.county} has {num} companies that need
         to report their emissions to the TCEQ.</p>
+        <h2>Emissions</h2>
+        <section>
+        <div className="emission-stats">
+          <div className="stat">
+            <p>Carbon Monoxide</p>
+            <p><b>{Math.round(co_tpy)}</b></p>
+          </div>
+          <div className="stat">
+            <p>Nitrous Oxide</p>
+            <p><b>{Math.round(nox_tpy)}</b></p>
+          </div>
+          <div className="stat">
+            <p>Lead</p>
+            <p><b>{Math.round(pb_tpy)}</b></p>
+          </div>
+          <div className="stat">
+            <p>Particular Matter (more than 10 microns)</p>
+            <p><b>{Math.round(pm_ten_tpy)}</b></p>
+          </div>
+          <div className="stat">
+            <p>Volatile Organic Compounds</p>
+            <p><b>{Math.round(voc_tpy)}</b></p>
+          </div>
+        </div>
+        </section>
       </div>
     );
     }
